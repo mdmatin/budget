cb24bdd63da5e5506682aebca636b7dc2f999388
Md Matin <120661418+mdmatin@users.noreply.github.com>
2026-09-17 16:00:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 /* Your Apps Script web app URL. Set this once; it survives every app update.
    Apps Script > Deploy > Manage deployments > copy the URL ending in /exec  */
-window.TRACKER_API = '';
+window.TRACKER_API = 'https://script.google.com/macros/s/AKfycbyQrgFU9DzAoQFRogXXT0b40zkDXRv7RHv9G58hm9EzG8k4r-5Xm2csl71EPA8vIeNnzQ/exec';
